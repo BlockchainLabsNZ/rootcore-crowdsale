b98e26dafe16703d9bd7cc26076ccf539322dd09
Bajayo <bajayo@gmail.com>
2017-09-07 18:06:12 +0300

    added a few more tests to Pausble

--- solidity/test/Pausable.js
+++ solidity/test/Pausable.js
@@ -22,6 +22,18 @@ contract('Pausable', (accounts) => {
         }
     });
 
+    it('should throw when non manager calls unPause function when contract is paused', async () => {
+        try {
+            let contract = await Pausable.new();
+            await contract.pause();
+            await contract.unpause({ from: accounts[1] });
+            assert(true, "didn't throw");
+        }
+        catch (error) {
+            return utils.ensureException(error);
+        }
+    });
+
     it('verifies that manager can pause when not paused', async () => {
         let contract = await Pausable.new();
         await contract.pause();
@@ -32,7 +44,6 @@ contract('Pausable', (accounts) => {
     it('should throw when manager calls Pause function when contract is paused', async () => {
         let contract = await Pausable.new();
         await contract.pause();
-        let paused = await contract.paused.call(); 
         try {
             await contract.pause();
             assert(true, "didn't throw");
@@ -41,4 +52,31 @@ contract('Pausable', (accounts) => {
             return utils.ensureException(error);
         }
     });
+
+    it('verifies that manager can unpause when contract is paused', async () => {
+        let contract = await Pausable.new();
+        try {
+            await contract.pause();
+            let paused = await contract.paused.call(); 
+            if(paused){
+                let unpaused = await contract.unpause(); 
+                assert.equal(paused, true);
+            }
+        }
+        catch (error) {
+            return utils.ensureException(error);
+        }
+    });
+
+    it('should throw when manager tries to unpause when contract is unpaused', async () => {
+        let contract = await Pausable.new();   
+        try {
+            await contract.unpause();
+            assert(true, "didn't throw");
+        }
+        catch (error) {
+            return utils.ensureException(error);
+        }
+    });
+
 });

914b65a4618b78e88fd92f86e1ccae5d3ade4f61
Bajayo <bajayo@gmail.com>
2017-09-17 18:22:32 +0300

    Truffle should only deploy CrowdsaleController. Working on the main net deployer.

--- solidity/migrations/2_deploy_contracts.js
+++ solidity/migrations/2_deploy_contracts.js
@@ -13,23 +13,16 @@ const CrowdsaleController = artifacts.require('CrowdsaleController.sol');
 
 module.exports = async (deployer) => {
     console.log("network: " + web3.version.network);
-    await deployer.deploy(Utils);
-    await deployer.deploy(Owned);
-    await deployer.deploy(Managed);
-    await deployer.deploy(Pausable);
-    await deployer.deploy(TokenHolder);
-    await deployer.deploy(ERC20Token, 'DummyToken', 'DUM', 0);
-    await deployer.deploy(SmartToken, 'Token1', 'TKN1', 18);
-    await deployer.deploy(SmartTokenController, SmartToken.address);
-    await deployer.deploy(CrowdsaleController, 4102444800, '0x125');
+    // await deployer.deploy(Utils);
+    // await deployer.deploy(Owned);
+    // await deployer.deploy(Managed);
+    // await deployer.deploy(Pausable);
+    // await deployer.deploy(TokenHolder);
+    // await deployer.deploy(ERC20Token, 'DummyToken', 'DUM', 0);
+    //await deployer.deploy(SmartToken, 'Token1', 'TKN1', 18);
+    // await deployer.deploy(SmartTokenController, SmartToken.address);
+    await deployer.deploy(CrowdsaleController, 1509537600, '0x125');
 };
 
 
-/************************************************
-TODO:
-1 - add network object to deployments
-2 - create a full deployment for testRPC and unit tests (this is what we have now)
-3 - create a dedicated testnet and main net deployments which will only deploy the crowdsale with relevant args.
-4 - decide if we will use truffle for the production deploment.
-*************************************************/  
 
